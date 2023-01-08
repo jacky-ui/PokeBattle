d417f8efbf68484ce06ba35c9d70bf0ef99aae2f
jacky-ui <jacky.du.004@gmail.com>
2023-01-07 21:56:30 -0500

    create eventListener to button and have it disappear when clicked

--- script/index-page.js
+++ script/index-page.js
@@ -1,3 +1,10 @@
 // Render background image when page starts up
 let body = document.querySelector("body");
 body.classList.add("background--initial");
+
+let button = document.querySelector(".button__start");
+let buttonParent = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+    buttonParent.classList.add("button__none");
+});
